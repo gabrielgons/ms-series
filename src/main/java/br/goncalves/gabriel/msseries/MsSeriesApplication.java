849f84bb6ba2f03e5c2b74ce4b1e7d7d79a38ce1
Gabriel Gonçalves <51233554+gabrielgons@users.noreply.github.com>
2023-10-01 12:42:56 -0300

    Commit inicial

---
+++ src/main/java/br/goncalves/gabriel/msseries/MsSeriesApplication.java
@@ -0,0 +1,13 @@
+package br.goncalves.gabriel.msseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSeriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/goncalves/gabriel/msseries/MsSeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.goncalves.gabriel.msseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
